a80194e87b8d423385081c662d693910ffbf238d
MegaOrion <biryuk3333@gmail.com>
2020-05-13 10:14:56 +0300

    hw15ex2

--- ex1.js
+++ ex1.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const request =  require('request');
 
-let HTML = fs.readFileSync('index.html')
+let HTML = fs.readFileSync('index.html');
 
 http.createServer(function(req, res) {
     if (req.url === '/') {
@@ -9,14 +10,26 @@ http.createServer(function(req, res) {
     } else if (req.url === '/contact') {
         res.writeHead(200, {'Content-type':'text/html; charset-utf8'});
         res.end(HTML);
+        console.log(HTML);
     } else if (req.url === '/about') {
         console.log(req.method);
         console.log(req.headers);
         console.log(req.url);
+    } else if (req.url === '/courses') {
+
+        //ДЗ 15, 2 задание
+
+        request.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3", function(error, response, body) {
+            response.on('data', (url) => {
+                fs.writeFileSync('index.html',url)
+            });
+        })
+        res.writeHead(200, {'Content-type':'text/html; charset-utf8'});
+        res.end(HTML);
     } else {
         res.write('lalala');
     }
     res.end();
 }).listen(3001,  function() {
-    console.log('Server at http://localhost:3001');
+    console.log('Server at http://localhost:3001/courses');
 })
--- ex3.js
+++ ex3.js
@@ -1,6 +1,8 @@
 const request = require("request");
 
-  
-request.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3", (err, res, body) => {   
-    console.log(JSON.parse(body));
+let data =
+
+request.get("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3", (err, res, body) => {
+    return body;   
 });
+console.log(data); 
